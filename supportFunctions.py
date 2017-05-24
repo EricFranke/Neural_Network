4b42089391b29d7a561f8dd73b7eec1f3d543111
Eric Franke <Eric.Franke1987@gmail.com>
2017-05-24 16:48:58 +0200

    Improved the format of the return values of importMNIST

--- supportFunctions.py
+++ supportFunctions.py
@@ -21,23 +21,29 @@ def errorFunction(inputData, trainingData):
         
     return error
 
-# imports the dataset from the .csv-file and transforms it according to the ANN architecture
-def importMNIST(name):
+# imports the mnist csv-file and transforms it according to the ANN architecture
+# return values: 
+#    - dataMatrix: every row represents a 28-by-28 image (784 elements)
+#    - labelMatrix: every row represents the correct result for training purposes (10 elements - one for each number)
+def importMNIST(file):
     
     # import data
-    dataFile = open("./data/" + name,"r")
+    dataFile = open("./data/" + file,"r")
     fullData = dataFile.readlines()
     dataFile.close()
     
     # pre-allocate space
-    dataMatrix = np.zeros((len(fullData), len(fullData[0].split(","))))
+    dataMatrix = np.zeros((len(fullData), len(fullData[0].split(","))-1))
+    labelMatrix = np.zeros((len(fullData), 10)) + 0.1
     
     for i, currentDataSet in enumerate(fullData):
         # transforms imported data into matrix containing integer values
-        dataMatrix[i] = list(map(int,currentDataSet.split(",")))
+        currentDataSet = list(map(int,currentDataSet.split(",")))
+        
+        labelMatrix[i,currentDataSet[0]] = 0.999
+        dataMatrix[i] = currentDataSet[1:]
     
-    # integer range from [0 255] -> [~0.1 ~0.999] to fit the intervall of the sigmoid function
-    dataMatrix[:,1:] = ((dataMatrix[:,1:] / 255)  + 0.01) * 0.99
+    # integer range from [0 255] -> [~0.1 ~0.999] to fit the range of the sigmoid function
+    dataMatrix = ((dataMatrix / 255)  + 0.01) * 0.99
     
-    # dataMatrix[i] returns the i-th dataset with the correct result as the first element
-    return dataMatrix
+    return (dataMatrix, labelMatrix)
